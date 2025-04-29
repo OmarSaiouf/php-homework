5f911bfd6bc1afcf7a8b9d397a9f9af05bb98551
Omar Saiouf <homar0083@gmail.com>
2025-04-29 12:17:10 +0300

    .

--- Models/product.php
+++ Models/product.php
@@ -1,12 +1,12 @@
 <?php
 class Product extends Base_model
 {
-    protected $table_name = "Products";
+    protected $table_name = "products";
     protected $columns = [
         "id INT(11) AUTO_INCREMENT PRIMARY KEY",
-        "name VARCHAR(255) NOT NULL",
-        "email VARCHAR(255) NOT NULL",
-        "password VARCHAR(255) NOT NULL",
-        "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP",
+        "product_name VARCHAR(255) NOT NULL",
+        "price DECIMAL(10,2) NOT NULL",
+        "description TEXT",
+        "image VARCHAR(255)"
     ];
 }
