d28463606521a62a446f10b43c33d1bc5aecc8cb
Omar Saiouf <homar0083@gmail.com>
2025-04-29 10:23:20 +0300

    تجهيز ال configuration

--- Helper/inital.php
+++ Helper/inital.php
@@ -1,20 +1,9 @@
 <?php
+include_once 'config.php';
 include_once 'database_tools/base_model.php';
+include_once 'Models/user.php';
 
 
 // -----------------------------------------------------------
 // -------------------الاتصال بقاعدة الباينات----------------
 // -----------------------------------------------------------
-
-class User extends Base_model
-{
-    protected $table_name = "users5";
-    protected $columns = [
-        "id INT(11) AUTO_INCREMENT PRIMARY KEY",
-        "name VARCHAR(255) NOT NULL",
-        "email VARCHAR(255) NOT NULL",
-        "password VARCHAR(255) NOT NULL",
-        "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP",
-    ];
-    
-}
