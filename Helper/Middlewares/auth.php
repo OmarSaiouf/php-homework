f5af96cdeea1e2710ad93e6220e778003808d02f
Omar Saiouf <homar0083@gmail.com>
2025-05-01 00:18:34 +0300

    .

--- Helper/Middlewares/auth.php
+++ Helper/Middlewares/auth.php
@@ -1,16 +1,18 @@
-<?php 
+<?php
 
-function authMiddleware($requireAdmin = false){
-    
-    if(isset($_SESSION['user'])){
+
+function authMiddleware($requireAdmin = false)
+{
+
+    if (isset($_SESSION['user'])) {
         $user = User::get_data_by_id($_SESSION['user']['id']);
-        if(!$user){
+        if (!$user) {
             go('login');
         }
-        if($requireAdmin && $user['role'] != 'admin'){
+        if ($requireAdmin && $user['role'] != 'admin') {
             go('404');
         }
-    }else{
+    } else {
         go('login');
     }
 }
